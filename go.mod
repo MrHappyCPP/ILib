9053190d9f28b4d87e0f502ad0703cf655793aa2
Joerg Froehlich <joerg.froehlich@rewe-digital.com>
2020-05-29 08:35:07 +0200

    depedency to google-cmp 4.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MrHappyCPP/ilib/v2
 
 go 1.14
+
+require github.com/google/go-cmp v0.4.0
--- ilib.go
+++ ilib.go
@@ -1,7 +1,12 @@
 package krassedinge
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/go-cmp/cmp"
+)
 
 func DestroyMe() {
 	fmt.Println("Hello from ilib/krasseDinge/DestroyMe Version 2.1.0")
+	result := cmp.Diff("","")
+	fmt.Println(result)
 }
